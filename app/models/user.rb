f73b98bea492c71b7e40befe33771f35041ece5c
Diogo Cardoso <dikx@netcabo.pt>
2013-03-03 18:14:56 +0100

    Added Simple_form, Attention there is a error

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :first_name, :last_name, :profile_name, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+has_many :statuses
+
+  def full_name
+first_name + " " + last_name
+  end
+
 end
